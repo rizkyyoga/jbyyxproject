39495b4fa846a3acb195792700935d5cd673d564
j_byyx <j_byyx@yahoo.com>
2009-07-16 14:54:17 +0000

    +Ally of Justice Catastor

--- src/version.py
+++ src/version.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-__appname__ = 'moose'
-__version__ = '0.1.5 2009.07.16 17:19 3470 cards'
+__version__ = '0.1.5 2009.07.16 17:46 3471 cards'
 __changelog__ = '0.1.5 More cards. Synchro Support. Bugfixes. And new look :) '
 
 def GetName(): return __name__
