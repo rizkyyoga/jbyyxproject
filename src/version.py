8b2c86b43320345971b2bad3940cfcc8aec59d8f
jbyyxx <jbyyxx@b52c2014-5cf8-11de-8abc-d1c337b90d21>
2009-08-20 17:55:24 +0000

    +10 cards some images and name changes...

--- src/version.py
+++ src/version.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-__appname__ = 'moose'
-__version__ = '0.1.5A 2009.08.09 14:14 3488 cards'
+__version__ = '0.1.5A 2009.08.20 20:50 3498 cards'
 __changelog__ = '0.1.5 More cards. Synchro Support. Bugfixes. And new look :) '
 
 def GetName(): return __name__
