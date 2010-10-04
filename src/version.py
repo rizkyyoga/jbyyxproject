cca993811042b057f7d4b46a246588010fbb0e11
jbyyxx <jbyyxx@b52c2014-5cf8-11de-8abc-d1c337b90d21>
2010-10-04 08:37:18 +0000

    Drafts for ChangeControl function. New cards and preparations for the new release.

--- src/version.py
+++ src/version.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-__appname__ = 'moose'
-__version__ = '0.1.5A 2010.02.13 17:00 3659 cards'
+__version__ = '0.1.5B 2010.10.04 11:14 3968 cards'
 __changelog__ = '0.1.5 More cards. Synchro Support. Bugfixes. And new look :) '
 
 def GetName(): return __name__
